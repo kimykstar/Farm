925431a7486aa89230dfb2361a174892dd7b1896
김영관 <kimyogun78@naver.com>
2023-07-20 16:24:02 +0900

    modify : 게시물 작성 통신 Controller완성

--- src/main/java/com/Hanium/Farm/Farm/Controller/CommunityController.java
+++ src/main/java/com/Hanium/Farm/Farm/Controller/CommunityController.java
@@ -1,13 +1,12 @@
 package com.Hanium.Farm.Farm.Controller;
 
+import com.Hanium.Farm.Farm.Domain.Review;
 import com.Hanium.Farm.Farm.Service.CommunityService;
-import jakarta.annotation.Resource;
-import jakarta.servlet.ServletInputStream;
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 public class CommunityController {
@@ -27,39 +27,37 @@ public class CommunityController {
         this.communityService = communityService;
     }
 
-    @PostMapping("/regist")
-    public String registReview(HttpServletRequest request) throws IOException {
-        String result = "False";
-        System.out.println("regist");
-        try{
-            InputStream inputStream = request.getInputStream();
-            String message = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-            if(message != null)
-                result = "true";
-            System.out.println(message);
-        }catch(IOException e){
-            log.info("register Error");
-        }
-        return result;
-    }
+    @PostMapping("regist")
+    public String registPost(@RequestParam("image")MultipartFile image, @RequestParam("review") String review) throws IOException {
+        if(!image.isEmpty()) { // 이미지를 등록했다면
+            String filePath = "src/main/resources/static/";
+            byte[] imageData = image.getBytes();
+            if(imageData != null) {
+                try{
+                    byte[] decodedBytes = Base64.getDecoder().decode(image.getOriginalFilename()); // Base65로부터 디코딩 함
+                    String originalImageName = new String(decodedBytes, StandardCharsets.UTF_8); // UTF-8로부터 디코딩함
 
-    @PostMapping("image")
-    public String registImage(HttpServletRequest request) throws IOException {
-
-        System.out.println("image");
-        byte[] imageData = request.getInputStream().readAllBytes();
-        String filePath = "src/main/resources/static";
-
-        if(imageData != null) {
-            try{
-                FileOutputStream fos = new FileOutputStream(filePath + "/image.jpg");
-                fos.write(imageData);
-            }catch(Exception e){
-                e.printStackTrace();
+                    FileOutputStream fos = new FileOutputStream(filePath + originalImageName);
+                    System.out.println("File Name : " + originalImageName);
+                    fos.write(imageData);
+                    Gson gson = new Gson();
+                    Review review_content = gson.fromJson(review, Review.class); // json으로부터 Review객체 생성
+                    System.out.println("이미지 업로드 성공");
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }else{
+                System.out.println("False");
             }
-        }else{
-            System.out.println("False");
         }
+        else
+            System.out.println("image is Null");
+
+        if(review != null && !review.isEmpty()){
+            System.out.println(review);
+            System.out.println("Review Info getted Successly");
+        }
+
         return "true";
     }
 
