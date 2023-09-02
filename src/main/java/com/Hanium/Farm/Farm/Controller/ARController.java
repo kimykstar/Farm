08185abfeca70e90419adda4f8de6d88a6839ad1
김영관 <kimyogun78@naver.com>
2023-09-02 20:00:24 +0900

    add : AR image return

---
+++ src/main/java/com/Hanium/Farm/Farm/Controller/ARController.java
@@ -0,0 +1,26 @@
+package com.Hanium.Farm.Farm.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+public class ARController {
+
+    @GetMapping(value = "arimage")
+    public String pushARImage(@RequestParam("fruit_name") String fruit_name) throws IOException {
+        String gltfPath = "src/main/resources/static/" + fruit_name + ".gltf";
+
+       String gltfJson = new String(Files.readAllBytes(Paths.get(gltfPath)), StandardCharsets.UTF_8);
+       return gltfJson;
+    }
+
+}
