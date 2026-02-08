2a577f52659da1c1c3c8ec846e600a5d1c173527
Younggwan <kimyogun78@naver.com>
2026-02-08 23:36:47 +0900

    feat: 로그인 실패 예외 클래스 추가

---
+++ src/main/java/com/Hanium/Farm/Farm/Exception/LoginFailException.java
@@ -0,0 +1,8 @@
+package com.Hanium.Farm.Farm.Exception;
+
+import lombok.Getter;
+
+@Getter
+public class LoginFailException extends RuntimeException{
+    private final String message = "아이디 혹은 비밀번호가 올바르지 않습니다.";
+}
