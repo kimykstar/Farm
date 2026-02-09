7cbb34a94e5602a66229053e86783ab03295b8d8
Younggwan <kimyogun78@naver.com>
2026-02-09 12:02:15 +0900

    test: 로그인 실패 파라미터테스트 추가

--- src/test/java/com/Hanium/Farm/Farm/Controller/MemberControllerTest.java
+++ src/test/java/com/Hanium/Farm/Farm/Controller/MemberControllerTest.java
@@ -1,8 +1,13 @@
 package com.Hanium.Farm.Farm.Controller;
 
+import com.Hanium.Farm.Farm.Dto.LoginRequest;
 import com.Hanium.Farm.Farm.Service.MemberService;
 import com.Hanium.Farm.Farm.Dto.AuthTokens;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -21,6 +27,8 @@ public class MemberControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private MemberService memberService;
@@ -64,8 +72,29 @@ public class MemberControllerTest {
                 .andExpect(jsonPath("$.message").value("아이디 혹은 비밀번호가 올바르지 않습니다."));
     }
 
-    @Test
-    void 유저_회원가입_성공() throws Exception {
+    @ParameterizedTest
+    @MethodSource("invalidLoginParams")
+    void 유저_로그인_파라미터로인한_실패(String id, String pw) throws Exception {
+        LoginRequest request = new LoginRequest(id, pw);
 
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.message").value("아이디 혹은 비밀번호가 누락되었습니다."));
     }
+
+    private static Stream<Arguments> invalidLoginParams() {
+        return Stream.of(
+                Arguments.of("kimyogun78@naver.com", null),
+                Arguments.of(null, "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"),
+                Arguments.of("", "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"),
+                Arguments.of("kimyogun78@naver.com", "")
+        );
+    }
+
+
 }
