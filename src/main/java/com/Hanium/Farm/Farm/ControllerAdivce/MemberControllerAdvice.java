d6ac863c463926db9bc8b94753d1626fb39ce942
Younggwan <kimyogun78@naver.com>
2026-02-09 12:44:29 +0900

    feat: Valid 예외 처리 Handler추가

--- src/main/java/com/Hanium/Farm/Farm/ControllerAdivce/MemberControllerAdvice.java
+++ src/main/java/com/Hanium/Farm/Farm/ControllerAdivce/MemberControllerAdvice.java
@@ -3,6 +3,7 @@ package com.Hanium.Farm.Farm.ControllerAdivce;
 import com.Hanium.Farm.Farm.Dto.LoginFailResponse;
 import com.Hanium.Farm.Farm.Excpetion.LoginFailException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,4 +17,9 @@ public class MemberControllerAdvice {
         return new LoginFailResponse(e.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public LoginFailResponse handleFailLoginByInvalidParameter(MethodArgumentNotValidException e) {
+        return new LoginFailResponse("아이디 혹은 비밀번호가 누락되었습니다.");
+    }
 }
