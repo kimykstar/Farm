f8613f3ede857751f928a37359584f4880637e54
Younggwan <kimyogun78@naver.com>
2026-02-10 22:21:17 +0900

    test: 회원가입 Unit Test

--- src/test/java/com/Hanium/Farm/Farm/Controller/MemberControllerTest.java
+++ src/test/java/com/Hanium/Farm/Farm/Controller/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package com.Hanium.Farm.Farm.Controller;
 
 import com.Hanium.Farm.Farm.Dto.LoginRequest;
+import com.Hanium.Farm.Farm.Dto.SignUpRequest;
 import com.Hanium.Farm.Farm.Service.MemberService;
 import com.Hanium.Farm.Farm.Dto.AuthTokens;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -35,7 +36,7 @@ public class MemberControllerTest {
 
     @Test
     void 유저_로그인_성공() throws Exception {
-        given(memberService.login(any(), any()))
+        given(memberService.login(any()))
                 .willReturn(Optional.of(new AuthTokens("access", "refresh")));
 
         mockMvc.perform(post("/login")
@@ -56,7 +57,7 @@ public class MemberControllerTest {
 
     @Test
     void 유저_로그인_실패() throws Exception {
-        given(memberService.login(any(), any()))
+        given(memberService.login(any()))
                 .willReturn(Optional.empty());
 
         mockMvc.perform(post("/login")
@@ -95,5 +96,79 @@ public class MemberControllerTest {
         );
     }
 
+    @Test
+    void 유저_회원가입_성공() throws Exception {
+        String id = "kimyogun78@naver.com";
+        String pw = "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f";
+        String name = "김영관";
+        String phone = "010-0000-0000";
+        int age = 27;
 
+        SignUpRequest request = new SignUpRequest(id, pw, name, phone, age);
+        given(memberService.join(any()))
+                .willReturn(true);
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.message").value("회원가입에 성공했습니다."));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidSignUpParams")
+    void 유저_회원가입_파라미터_테스트(
+            String id,
+            String pw,
+            String name,
+            String phone,
+            int age) throws Exception {
+        SignUpRequest request = new SignUpRequest(id, pw, name, phone, age);
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").exists());
+    }
+
+
+
+    private static Stream<Arguments> invalidSignUpParams() {
+        return Stream.of(
+                Arguments.of("kimyogun78@naver.com",
+                        "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f",
+                        "김영관",
+                        "010-0000-0000",
+                        -1
+                ),
+                Arguments.of("kimyogun78@naver.com",
+                        "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f",
+                        "김영관",
+                        null,
+                        27
+                ),
+                Arguments.of("kimyogun78@naver.com",
+                        "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f",
+                        null,
+                        "010-0000-0000",
+                        27
+                ),Arguments.of("kimyogun78@naver.com",
+                        null,
+                        "김영관",
+                        "010-0000-0000",
+                        27
+                ),Arguments.of(null,
+                        "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f",
+                        "김영관",
+                        "010-0000-0000",
+                        27
+                )
+        );
+    }
 }
