227304c98f08bc61a439fb439762da529236774b
Younggwan <kimyogun78@naver.com>
2026-02-21 22:19:33 +0900

    test: AccessToken 만료 및 무결성 위반 테스트 코드 작성

--- src/test/java/com/Hanium/Farm/Farm/Controller/FruitControllerTest.java
+++ src/test/java/com/Hanium/Farm/Farm/Controller/FruitControllerTest.java
@@ -1,38 +1,49 @@
 package com.Hanium.Farm.Farm.Controller;
 
-import com.Hanium.Farm.Farm.Advices.AuthAdvice;
 import com.Hanium.Farm.Farm.Components.JwtProvider;
-import com.Hanium.Farm.Farm.Interceptors.TokenInterceptor;
 import com.Hanium.Farm.Farm.Service.FruitService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.security.Key;
+import java.util.Date;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class FruitControllerTest {
+
+    @Autowired
     private MockMvc mockMvc;
-    @Mock
-    private JwtProvider jwtProvider;
-    @Mock
+    @MockBean
     private FruitService fruitService;
+    @Value("${spring.jwt.token.secret.key}")
+    private String privateKey;
+    @Autowired
+    JwtProvider jwtProvider;
+
+    private Key key;
 
     @BeforeEach
     void setUp() {
-        FruitController fruitController = new FruitController(fruitService);
-
-        mockMvc = MockMvcBuilders.standaloneSetup(fruitController)
-                .setControllerAdvice(new AuthAdvice())
-                .addInterceptors(new TokenInterceptor(jwtProvider))
-                .build();
+        this.key = Keys.hmacShaKeyFor(privateKey.getBytes());
     }
+
     @Test
     void 제외된_경로는_인터셉터_경유하지않는다() throws Exception {
         mockMvc.perform(get("/"))
@@ -44,5 +55,34 @@ class FruitControllerTest {
         mockMvc.perform(get("/api/search"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.message").value("로그인이 필요합니다."));
+    }
+
+    @Test
+    void 토큰_만료_테스트() throws Exception {
+        Date now = new Date();
+        Claims claims = Jwts.claims()
+                        .setSubject("farm")
+                        .setIssuedAt(new Date(now.getTime() - 1000))
+                        .setExpiration(new Date(now.getTime() - 500));
+        String expiredAccessToken = Jwts.builder().setClaims(claims)
+                            .claim("userId", "111")
+                            .signWith(key, SignatureAlgorithm.HS256)
+                            .compact();
+
+        mockMvc.perform(get("/api/search")
+                .header("Authorization", expiredAccessToken))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").value("엑세스 토큰이 만료되었습니다."));
+    }
+
+    @Test
+    void 토큰_무결성_손상_테스트() throws Exception {
+        String accessToken = jwtProvider.createAccessToken("user");
+        String tamperedToken = accessToken + "hacked";
+
+        mockMvc.perform(get("/api/search")
+                .header("Authorization", tamperedToken))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").value("시그니쳐 무결성 위반"));
     }
 }
