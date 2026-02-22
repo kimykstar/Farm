1f2746640e8b56667ec7391034d9437fabf3a0ad
Younggwan <kimyogun78@naver.com>
2026-02-22 21:09:04 +0900

    refactor: WebMvcTest 활용 테스트 동작 간소화

--- src/test/java/com/Hanium/Farm/Farm/Controller/FruitControllerTest.java
+++ src/test/java/com/Hanium/Farm/Farm/Controller/FruitControllerTest.java
@@ -11,8 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,9 +24,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(FruitController.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import({JwtProvider.class})
 class FruitControllerTest {
 
     @Autowired
